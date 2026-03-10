ef519343c6cc2f68d3b1337233f622069845bf91
vonvit <sandrineviturn@gmail.com>
2026-03-10 20:15:24 -0300

    Solução Muito Ruim do exercicio pratico!

--- excecoes-personalizadas/src/main/java/application/Program.java
+++ excecoes-personalizadas/src/main/java/application/Program.java
@@ -1,15 +1,54 @@
 package application;
 
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-    void main(){
+    void main() throws ParseException {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in data (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.print("Check-out data (dd/MM/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
 
+        if(!checkOut.after(checkIn)){
+            System.out.println("Error in reservation: Check-out date must be after check-in date ");
+        }
+        else{
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
 
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+            System.out.print("Check-in data (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out data (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+
+            //SOLUÇÃO MUITO RUIM:
+            Date now = new Date();
+            if(checkIn.before(now) || checkOut.before(now)){
+                System.out.println("Error in reservation: Reservation dates for updates must be future ");
+            }
+            else if(!checkOut.after(checkIn)){
+                System.out.println("Error in reservation: Check-out date must be after check-in date ");
+            }
+            else{
+                reservation.updateDates(checkIn, checkOut);
+                System.out.println("Reservation: " + reservation);
+            }
+        }
+        sc.close();
     }
 }
